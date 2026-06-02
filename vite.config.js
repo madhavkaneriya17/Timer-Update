0edcef911afb6d333e3a743dce3a4a9f677c1faf
madhavkaneriya17 <YOUR_GITHUB_EMAIL>
2026-06-02 17:35:57 +0530

    fix registry mime type for windows

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'configure-mime-types',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          const url = req.url.split('?')[0];
+          if (url.endsWith('.jsx')) {
+            res.setHeader('Content-Type', 'application/javascript');
+          }
+          next();
+        });
+      }
+    }
+  ],
   server: {
     host: true,
   }
